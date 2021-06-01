37a13b10fa7cc28deadc744c9de33e0be2340b8e
Cem Nisan <2017417057@ogr.cu.edu.tr>
2021-06-02 01:29:40 +0300

    search endpoint added

--- src/controller/room.js
+++ src/controller/room.js
@@ -3,10 +3,10 @@ import fs from "fs";
 
 export const getRooms = async (req, res) => {
   const page = req.query.page || 1;
-  const dataPerPage = 2;
-  
+  const dataPerPage = 2; // Amount of data in the page
+
   try {
-    const totalData = await Room.countDocuments();
+    const totalData = await Room.countDocuments(); // Total data.
     const rooms = await Room.find({})
       .sort("-roomName")
       .skip((page - 1) * dataPerPage)
@@ -14,12 +14,30 @@ export const getRooms = async (req, res) => {
 
     res.status(200).json({
       rooms,
-      pages:Math.ceil(totalData / dataPerPage),
-      current:page
+      pages: Math.ceil(totalData / dataPerPage),
+      current: page,
     });
-
   } catch (err) {
     res.status(404).json({ message: err });
+  }
+};
+
+export const searchRooom = async (req, res) => {
+  const { q } = req.query;
+  
+  try {
+    if (q) {
+      // Search the database for information from query.
+      const data = await Room.find({ roomName: new RegExp(q, "i") });
+
+      if (data.length < 1) {
+        res.status(404).json({ message: `No results found for this ${q}` });
+      } else {
+        res.status(200).json(data);
+      }
+    }
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
 
--- src/routers/room.js
+++ src/routers/room.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getRooms, addRoom, updateReservation } from "../controller/room";
+import { getRooms, addRoom, updateReservation, searchRooom } from "../controller/room";
 import uploadImage  from "../utils/multer";
 
 const router = express.Router();
 
 router.get("/v1/rooms", getRooms);
+router.get('/v1/rooms/search',searchRooom);
 router.post("/v1/rooms", uploadImage, addRoom);
 router.put("/v1/rooms/:id",updateReservation);
 
