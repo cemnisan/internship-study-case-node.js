c835b5321a09e53b107ed54c7d8c6bf1f0180899
Cem Nisan <2017417057@ogr.cu.edu.tr>
2021-06-01 21:01:54 +0300

    pagination

--- src/controller/room.js
+++ src/controller/room.js
@@ -1,10 +1,23 @@
 import Room from "../model/Room";
-import fs from 'fs';
+import fs from "fs";
 
-export const getRooms = async (_req, res) => {
+export const getRooms = async (req, res) => {
+  const page = req.query.page || 1;
+  const dataPerPage = 2;
+  
   try {
-    const rooms = await Room.find({});
-    res.status(200).json(rooms);
+    const totalData = await Room.countDocuments();
+    const rooms = await Room.find({})
+      .sort("-roomName")
+      .skip((page - 1) * dataPerPage)
+      .limit(dataPerPage);
+
+    res.status(200).json({
+      rooms,
+      pages:Math.ceil(totalData / dataPerPage),
+      current:page
+    });
+
   } catch (err) {
     res.status(404).json({ message: err });
   }
@@ -14,8 +27,9 @@ export const addRoom = async (req, res) => {
   try {
     const newRoom = new Room({
       ...req.body,
-      roomImage: req.file ? req.file.path : null
+      roomImage: req.file ? req.file.path : null,
     });
+
     const results = await newRoom.save();
     res.status(201).json(results);
   } catch (err) {
@@ -44,12 +58,11 @@ export const deleteRoom = async (req, res) => {
     const { id } = req.params;
     const room = await Room.findOne({ _id: id });
 
-    const deleteImage = room.roomImage
+    const deleteImage = room.roomImage;
     fs.unlinkSync(deleteImage);
-  
+
     await Room.findByIdAndDelete(id);
     res.status(201).send({ message: "This room has been deleted." });
-
   } catch (err) {
     res.status(404).json({ message: err });
   }
--- src/index.js
+++ src/index.js
@@ -21,9 +21,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-
 app.use('/uploads',express.static(path.join(__dirname,'uploads')));
-console.log(path.join(__dirname,'uploads'))
+
 app.use("/api/swagger", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use("/api", roomRouter);
 app.use("/api/admin", adminRouter);
